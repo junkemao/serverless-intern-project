f6fc3b68546e73657385763607e11ad2dbbaf058
junkemao <junke001@e.ntu.edu.sg>
2024-07-29 17:49:59 +0800

    Create client.go

---
+++ demo/client.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "path/to/generated/proto/files"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("grpc-server.default.svc.cluster.local:80", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewExampleServiceClient(conn)
+
+	rate := 1
+	interval := time.Second / time.Duration(rate)
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Client context done")
+			return
+		case <-ticker.C:
+			req := &pb.FunctionRequest{} // Empty request
+			res, err := client.InvokeFunction(ctx, req)
+			if err != nil {
+				log.Fatalf("could not invoke: %v", err)
+			}
+			log.Printf("Response: %s", res.Message)
+		}
+	}
+}
